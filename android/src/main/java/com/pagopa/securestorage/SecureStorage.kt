15465f00f04de665fe1ce0ef7793724e11bab68a
RiccardoMolinari95 <riccardo95molinari@gmail.com>
2025-06-11 10:44:53 +0200

    chore: deleted throw excpetion for testing

--- android/src/main/java/com/pagopa/securestorage/SecureStorage.kt
+++ android/src/main/java/com/pagopa/securestorage/SecureStorage.kt
@@ -97,7 +97,6 @@ class SecureStorage private constructor(
    */
   fun get(key: String): ByteArray? {
     return runCatching {
-      throw IllegalStateException("Unrecognized file prefix")
       val file = AtomicFile(getFile(key))
       val data = file.readFully()
       check(data.size >= PREFIX_ENCRYPTED_SIZE) { "File must be bigger than $PREFIX_ENCRYPTED_SIZE bytes" }
